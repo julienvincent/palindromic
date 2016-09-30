46ad0ba1b1c99a7eb1c171092e0f8b6e5e253359
Julien Vincent <julienlucvincent@gmail.com>
2016-09-30 22:33:33 +0200

    optimized calc recursion to skip large iteration cycles

--- p1.hs
+++ p1.hs
@@ -1,11 +1,20 @@
 #!/usr/bin/env stack
 -- stack --install-ghc runghc
 
+{-
+    let dist(i) be the value of the distance tothe next palindrome from i
+
+    Target 1)   Prove that sum dist(i) of all integers > 0 and < 1000 000 equates to 495076635
+    Target 2)   Determine the sum of dist(i) of all integers > 0 and < 1000 000 000 000
+-}
+
+-- Check if an int is a palindrome
 check :: Int -> Bool
 check i = do
     let s = show i :: String
     s == reverse s
 
+-- Determine distance to next palindrome from i
 dist :: Int -> Int -> Int
 dist i acc = do
     let _i = i + acc
@@ -13,18 +22,35 @@ dist i acc = do
         then acc
         else dist i (acc + 1)
 
-calc :: Int -> Int -> Int -> Int -> Int
-calc start end acc cache = do
-    if start == end
+_sum :: Int -> Int
+_sum x = (x * x + x) `div` 2
+
+calc :: Int -> Int -> Int -> Int
+calc start end acc = do
+    if start >= end
+        -- We are done. return the sum
         then acc
         else do
-            let distance = if cache < 0
-                then dist start 0
-                else cache
-            calc (start + 1) end (acc + distance) (distance - 1)
+            -- Get the distance to the next palindome
+            let distance = dist start 0
+            let nextIteration = start + distance
+
+            -- Distance will be 0 for single digit palindromes. Skip them.
+            if distance == 0
+                then calc (start + 1) end acc
+                else if nextIteration > end
+                    -- Add all iterations until the end of the sequence
+                    then do
+                        let diff = nextIteration - end
+                        let totalSum = _sum distance
+                        let diffSum = _sum diff
+                        let nextAcc = totalSum - diffSum
+                        calc (start + nextIteration) end (acc + nextAcc)
+                    -- Add all iterations until the next palindrome
+                    else calc (nextIteration + 1) end (acc + (_sum distance))
 
 main :: IO()
 main = do
     print ("Iterations?:")
     runXTimes <- getLine
-    print ("result: " ++ show (calc 0 (read runXTimes :: Int) 0 (-1)) :: String)
+    print ("result: " ++ show (calc 0 (read runXTimes :: Int) 0) :: String)
